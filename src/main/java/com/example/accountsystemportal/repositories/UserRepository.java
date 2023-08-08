788a600da5c54359bdca489c073d3fc2c7773170
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-08 10:15:37 +0300

    feat(Repository): Create User repository

--- src/main/java/com/example/accountsystemportal/repositories/UserRepository.java
+++ src/main/java/com/example/accountsystemportal/repositories/UserRepository.java
@@ -1,4 +1,11 @@
 package com.example.accountsystemportal.repositories;
 
-public interface UserRepository {
+import com.example.accountsystemportal.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
 }
