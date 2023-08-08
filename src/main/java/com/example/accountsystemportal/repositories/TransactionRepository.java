a1be2d22123a69bc30fdd7d0373388fe6ad6a611
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-08 10:16:46 +0300

    feat(Repository): Create transaction repository

--- src/main/java/com/example/accountsystemportal/repositories/TransactionRepository.java
+++ src/main/java/com/example/accountsystemportal/repositories/TransactionRepository.java
@@ -1,4 +1,9 @@
 package com.example.accountsystemportal.repositories;
 
-public interface TransactionRepository {
+import com.example.accountsystemportal.entities.Transaction;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TransactionRepository extends JpaRepository<Transaction, Long> {
 }
