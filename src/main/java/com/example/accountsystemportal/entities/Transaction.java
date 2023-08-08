012ce441b851f753ee4bfc38d5919da9f6761365
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-08 10:04:39 +0300

    feat(Entity): Populate transaction model

--- src/main/java/com/example/accountsystemportal/entities/Transaction.java
+++ src/main/java/com/example/accountsystemportal/entities/Transaction.java
@@ -1,4 +1,31 @@
 package com.example.accountsystemportal.entities;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Entity
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Table(name = "Transactions")
 public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String transactionType;
+    private Integer transactionAmount;
+    @Column(nullable = false)
+    @JsonFormat(pattern="yyyy-MM-dd")
+    private LocalDate transactionDate;
+
+    @PrePersist
+    private void onCreate(){
+        transactionDate =  LocalDate.now();
+    }
 }
