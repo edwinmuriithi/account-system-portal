0ad9795a907ac00fdbb306ae4aaf132747a610cd
edwinmuriithi <kabuimuriithi@gmail.com>
2023-08-08 11:13:51 +0300

    feat(Controller): Update Controller

--- src/main/java/com/example/accountsystemportal/controller/UserController.java
+++ src/main/java/com/example/accountsystemportal/controller/UserController.java
@@ -1,9 +1,17 @@
 package com.example.accountsystemportal.controller;
 
+import com.example.accountsystemportal.services.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
+    @Autowired
+    private UserService userService;
+
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 }
